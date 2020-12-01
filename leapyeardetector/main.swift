8fb9cb10c2c1a80b69b552b8071a8968e1882e66
Kyle Dunlap <dunlap.w@gmail.com>
2020-12-01 04:53:59 -0500

    Initial commit for leap year detection; converted from Python script.

--- leapyeardetector/main.swift
+++ leapyeardetector/main.swift
@@ -4,8 +4,25 @@
 //
 //  Created by Kyle Dunlap on 12/1/20.
 //
+// Converted from Python 3 with no optimizations yet.
 
 import Foundation
 
-print("Hello, World!")
+func leapYearDetector(year: Int) -> String {
+    let isnot = "\(year) was not a leap year."
+    let isly = "\(year) was a leap year!"
+    if year % 4 == 0 && year % 100 != 0 {
+        return isly
+    } else if year % 400 == 0 {
+        return isly
+    } else {
+        return isnot
+    }
+}
 
+print("Type a year below.")
+if let typedInput = readLine() {
+    if let yearInput = Int(typedInput) {
+        print(leapYearDetector(year: yearInput))
+    }
+}
